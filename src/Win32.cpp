9cadb7c22268f6793d72181f819ff4770175ff02
ChicoSantos10 <francisco_mms@hotmail.com>
2023-04-25 22:26:43 +0100

    Added xaudio redistributable

--- src/Win32.cpp
+++ src/Win32.cpp
@@ -7,8 +7,11 @@
 #include <iostream>
 #include <xinput.h>
 #include <cmath>
-#include <xaudio2.h>
 #include <wrl\client.h>
+#include "XAudioRedist/xaudio2.h"
+#include "XAudioRedist/xaudio2fx.h"
+#include "XAudioRedist/xapofx.h"
+#include "XAudioRedist/xapo.h"
 
 LRESULT CALLBACK WindowMessageCallback(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 void InitConsole();
@@ -93,6 +96,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 	buffer.AudioBytes = wave.Format.nSamplesPerSec * wave.Format.nChannels * (wave.Format.wBitsPerSample / 8);
 	buffer.pAudioData = new BYTE[buffer.AudioBytes];
 	buffer.Flags = XAUDIO2_END_OF_STREAM;
+	buffer.LoopCount = XAUDIO2_LOOP_INFINITE;
 
 	// Populate the audio buffer with some simple sine wave data at 440 Hz.
 	for (DWORD i = 0; i < wave.Format.nSamplesPerSec; ++i)
@@ -105,13 +109,89 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 		((short*)buffer.pAudioData)[i * 2 + 1] = (short)(sinf(t*TwoPi*440.0f) * 30000.0f); // Right channel
 	}
 
+	IXAudio2SubmixVoice* pSubMixVoice;
+	hr = xAudio2->CreateSubmixVoice(&pSubMixVoice, 2, wave.Format.nSamplesPerSec, 0, 0, nullptr, nullptr);
+	if (FAILED(hr))
+	{
+		OutputDebugStringW(L"XAudio2 submix voice initialization failed");
+		return -1;
+	}
+
+	XAUDIO2_SEND_DESCRIPTOR sendDescriptors = { 0, pSubMixVoice };
+	XAUDIO2_VOICE_SENDS sendList = { 1, &sendDescriptors };
+
+	IUnknown *reverbFx;
+	if (FAILED(hr = XAudio2CreateReverb(&reverbFx)))
+	{
+		OutputDebugStringW(L"XAudio2 reverb initialization failed");
+		return -1;
+	}
+
+	XAUDIO2_EFFECT_DESCRIPTOR descriptor = {reverbFx, true, 1 };
+	XAUDIO2_EFFECT_CHAIN effectChain = { 1, &descriptor };
+	XAUDIO2FX_REVERB_PARAMETERS reverbParameters;
+	reverbParameters.ReflectionsDelay = XAUDIO2FX_REVERB_DEFAULT_REFLECTIONS_DELAY;
+	reverbParameters.ReverbDelay = XAUDIO2FX_REVERB_DEFAULT_REVERB_DELAY;
+	reverbParameters.RearDelay = XAUDIO2FX_REVERB_DEFAULT_REAR_DELAY;
+	reverbParameters.PositionLeft = XAUDIO2FX_REVERB_DEFAULT_POSITION;
+	reverbParameters.PositionRight = XAUDIO2FX_REVERB_DEFAULT_POSITION;
+	reverbParameters.PositionMatrixLeft = XAUDIO2FX_REVERB_DEFAULT_POSITION_MATRIX;
+	reverbParameters.PositionMatrixRight = XAUDIO2FX_REVERB_DEFAULT_POSITION_MATRIX;
+	reverbParameters.EarlyDiffusion = XAUDIO2FX_REVERB_DEFAULT_EARLY_DIFFUSION;
+	reverbParameters.LateDiffusion = XAUDIO2FX_REVERB_DEFAULT_LATE_DIFFUSION;
+	reverbParameters.LowEQGain = XAUDIO2FX_REVERB_DEFAULT_LOW_EQ_GAIN;
+	reverbParameters.LowEQCutoff = XAUDIO2FX_REVERB_DEFAULT_LOW_EQ_CUTOFF;
+	reverbParameters.HighEQGain = XAUDIO2FX_REVERB_DEFAULT_HIGH_EQ_GAIN;
+	reverbParameters.HighEQCutoff = XAUDIO2FX_REVERB_DEFAULT_HIGH_EQ_CUTOFF;
+	reverbParameters.RoomFilterFreq = XAUDIO2FX_REVERB_DEFAULT_ROOM_FILTER_FREQ;
+	reverbParameters.RoomFilterMain = XAUDIO2FX_REVERB_DEFAULT_ROOM_FILTER_MAIN;
+	reverbParameters.RoomFilterHF = XAUDIO2FX_REVERB_DEFAULT_ROOM_FILTER_HF;
+	reverbParameters.ReflectionsGain = XAUDIO2FX_REVERB_DEFAULT_REFLECTIONS_GAIN;
+	reverbParameters.ReverbGain = XAUDIO2FX_REVERB_DEFAULT_REVERB_GAIN;
+	reverbParameters.DecayTime = XAUDIO2FX_REVERB_DEFAULT_DECAY_TIME;
+	reverbParameters.Density = XAUDIO2FX_REVERB_DEFAULT_DENSITY;
+	reverbParameters.RoomSize = XAUDIO2FX_REVERB_DEFAULT_ROOM_SIZE;
+	reverbParameters.WetDryMix = XAUDIO2FX_REVERB_DEFAULT_WET_DRY_MIX;
+
+	IUnknown *echoFx;
+	CreateFX(CLSID_FXEcho, &echoFx);
+	XAUDIO2_EFFECT_DESCRIPTOR echoDescriptor = { echoFx, true, 1 };
+	XAUDIO2_EFFECT_CHAIN echoChain = { 1, &echoDescriptor };
+	FXECHO_PARAMETERS echoParameters;
+	echoParameters.WetDryMix = FXECHO_DEFAULT_WETDRYMIX * 2;
+	echoParameters.Feedback = FXECHO_DEFAULT_FEEDBACK * 2;
+	echoParameters.Delay = FXECHO_DEFAULT_DELAY * 2;
+
+	IUnknown *EQFx;
+	CreateFX(CLSID_FXEQ, &EQFx);
+	XAUDIO2_EFFECT_DESCRIPTOR EQDescriptor = { EQFx, true, 1 };
+	XAUDIO2_EFFECT_CHAIN EQChain = { 1, &EQDescriptor };
+	FXEQ_PARAMETERS EQParameters;
+	EQParameters.Gain0 = FXEQ_MIN_GAIN;
+	EQParameters.FrequencyCenter0 = FXEQ_MIN_FREQUENCY_CENTER;
+
 	IXAudio2SourceVoice* pSourceVoice;
-	hr = xAudio2->CreateSourceVoice(&pSourceVoice, (WAVEFORMATEX*)&wave);
+	hr = xAudio2->CreateSourceVoice(&pSourceVoice, (WAVEFORMATEX*)&wave, 0, XAUDIO2_DEFAULT_FREQ_RATIO, nullptr, &sendList, nullptr);
 	if (FAILED(hr))
 	{
 		OutputDebugStringW(L"XAudio2 source voice initialization failed");
 		return -1;
 	}
+
+	// Reverb
+	pSourceVoice->SetEffectChain(&effectChain);
+	pSourceVoice->SetEffectParameters(0, &reverbParameters, sizeof(reverbParameters));
+	pSourceVoice->EnableEffect(0);
+
+	// Echo
+	pSourceVoice->SetEffectChain(&echoChain);
+	pSourceVoice->SetEffectParameters(1, &echoParameters, sizeof(echoParameters));
+	pSourceVoice->EnableEffect(1);
+
+	// EQ
+	pSourceVoice->SetEffectChain(&EQChain);
+	pSourceVoice->SetEffectParameters(2, &EQParameters, sizeof(EQParameters));
+	pSourceVoice->EnableEffect(2);
 
 	hr = pSourceVoice->SubmitSourceBuffer(&buffer);
 	if (FAILED(hr))

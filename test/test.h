1319fdfe14da25d61c4a76856ab0bd1dfab2d577
ChicoSantos10 <francisco_mms@hotmail.com>
2025-08-18 17:14:54 +0100

    Updated license in all source files

--- test/test.h
+++ test/test.h
@@ -1,18 +1,20 @@
 /*  SSS Engine
-    Copyright (C) 2024  Francisco Santos
+    Copyright (C) 2025  Francisco Santos
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
 */
 
 #include <string>
@@ -36,7 +38,10 @@ namespace SSSTest
 
         TestData(int line, std::string file, function *test) : line{line}, file{std::move(file)}, test{test} {}
 
-        void operator()() const { test(); }
+        void operator()() const
+        {
+            test();
+        }
     };
 
     SSSENGINE_GLOBAL std::vector<TestData> Tests{};
@@ -45,9 +50,15 @@ namespace SSSTest
     class Test
     {
         public:
-        explicit Test(const TestData &data) { Add(data); }
+        explicit Test(const TestData &data)
+        {
+            Add(data);
+        }
 
-        static void Add(const TestData &data) { Tests.push_back(data); }
+        static void Add(const TestData &data)
+        {
+            Tests.push_back(data);
+        }
 
         static void Execute()
         {
